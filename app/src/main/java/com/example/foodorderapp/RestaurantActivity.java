d699d71e36897a85a251e5914f50ec892b582dc4
Michael Osborne <resonatortune@gmail.com>
2018-10-17 22:26:51 -0700

    Added RestaurantActivity

---
+++ app/src/main/java/com/example/foodorderapp/RestaurantActivity.java
@@ -0,0 +1,18 @@
+package com.example.foodorderapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * The homepage for when a restaurant/food worker logs in.
+ */
+public class RestaurantActivity extends AppCompatActivity {
+
+    // no extras out
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+    }
+}
