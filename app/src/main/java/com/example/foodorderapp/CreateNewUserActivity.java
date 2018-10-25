9f93f212d47413b52dc473a2499bd562f71b8b68
Michael Osborne <resonatortune@gmail.com>
2018-10-24 20:53:20 -0700

    Login and New User fully functioning.

--- app/src/main/java/com/example/foodorderapp/CreateNewUserActivity.java
+++ app/src/main/java/com/example/foodorderapp/CreateNewUserActivity.java
@@ -1,7 +1,15 @@
 package com.example.foodorderapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Activity to create a new user and select between if the account is a driver account or a customer
@@ -9,11 +17,30 @@ import android.os.Bundle;
  */
 public class CreateNewUserActivity extends AppCompatActivity {
 
-    // no extras out
+    public static final String EXTRA_ACCOUNT_CREATED = "com.example.foodorderapp.ACCOUNTCREATED";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+    }
+
+    public void createAccount(View view) {
+        // Get the text values
+        TextView email = (TextView) findViewById(R.id.email);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        String newEmail = email.getText().toString();
+        String newPassword = password.getText().toString();
+
+        // Create new Key value pair with the username and password
+        SharedPreferences sharedPref = getSharedPreferences("mySettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(newEmail, newPassword);
+        editor.commit();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(EXTRA_ACCOUNT_CREATED, "Account created!");
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/foodorderapp/LoginActivity.java
+++ app/src/main/java/com/example/foodorderapp/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.foodorderapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -62,6 +64,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private TextView mNotificationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,30 +72,41 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
+//        populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
+//        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+//                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+//                    attemptLogin();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
+//        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+//        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                attemptLogin();
+//            }
+//        });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+        mNotificationView = findViewById(R.id.display_text);
+
+        // Show Account Created Message
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(CreateNewUserActivity.EXTRA_ACCOUNT_CREATED);
+
+        if (message != null) {
+            mNotificationView.setVisibility(View.VISIBLE);
+            mNotificationView.setText(message);
+        }
+
     }
 
     private void populateAutoComplete() {
@@ -198,7 +212,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        //return password.length() > 4;
+        return true;
     }
 
     /**
@@ -351,6 +366,19 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     public void registerNewUser(View view) {
         Intent intent = new Intent(this, CreateNewUserActivity.class);
         startActivity(intent);
+    }
+
+    public void signIn(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("mySettings", Context.MODE_PRIVATE);
+        String password = sharedPref.getString(mEmailView.getText().toString(), null);
+        // We have an account with that matching password
+        if (password != null && password.equals(mPasswordView.getText().toString())) {
+            Intent intent = new Intent(this, FoodStoreSearchActivity.class);
+            startActivity(intent);
+        } else {
+            mNotificationView.setVisibility(View.VISIBLE);
+            mNotificationView.setText("INVALID LOGIN");
+        }
     }
 }
 
