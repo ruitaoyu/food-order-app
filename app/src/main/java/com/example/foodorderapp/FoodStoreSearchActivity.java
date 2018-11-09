a67a247d6ce3e0772043780f7bb547ef7fe1b577
ruitaoyu <ruitaoyu@uw.edu>
2018-11-09 15:18:09 -0800

    done with front end of food store search activity

--- app/src/main/java/com/example/foodorderapp/FoodStoreSearchActivity.java
+++ app/src/main/java/com/example/foodorderapp/FoodStoreSearchActivity.java
@@ -2,6 +2,8 @@ package com.example.foodorderapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Search all registered stores based on food or store name.
@@ -15,5 +17,22 @@ public class FoodStoreSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_store_search);
+
+
+        Spinner dropdown = findViewById(R.id.stateSpinner);
+
+        String[] states = new String[]{"AL", "AK", "AS","AZ", "AR", "CA","CO", "CT", "DE", "DC"
+                ,"FM", "FL", "GA","GU", "HI", "ID","IL", "IN", "IA", "KS"
+                ,"KY", "LA", "ME","MH", "MD", "MA","MI", "MN", "MS", "MO"
+                ,"MT", "NE", "NV","NH", "NJ", "NM","NY", "NC", "ND", "MP"
+                ,"OH", "OK", "OR","PW", "PA", "PR","RI", "SC", "SD", "TN"
+                ,"TX", "UT", "VT","VI", "VA", "WA","WV", "WI", "WY"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this
+                , android.R.layout.simple_spinner_dropdown_item, states);
+
+
+        dropdown.setAdapter(adapter);
+
     }
 }
